9ffeac364e1ac4c3a1ed4b4fd7e3145b720c6a9e
Thomas Preusse <tpreusse@users.noreply.github.com>
2019-04-24 17:09:13 +0200

    add workaround to avoid backend crash on travis

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,7 @@
 
 cd apps/backends
-PORT=5000 SERVER=republik yarn start &
+# TRAVIS= workaround: https://github.com/orbiting/backends/issues/246
+TRAVIS= PORT=5000 SERVER=republik yarn start &
 cd ../..
 cd apps/frontend
 npm run dev &

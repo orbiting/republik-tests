9cbd69f48b12877c8c02b4e19db64764b169a8b0
Thomas Preusse <tpreusse@users.noreply.github.com>
2019-06-21 11:14:06 +0200

    fixes for CI and workaround no document index
    feed does not request documents when not logged in, front does nowadays

--- utils.js
+++ utils.js
@@ -3,7 +3,7 @@ import { waitForReact } from 'testcafe-react-selectors';
 
 const setUp = () => {
   fixture`Republik`
-    .page`${process.env.TARGET_URL || 'http://localhost:3010/'}`
+    .page`${process.env.TARGET_URL || 'http://localhost:3010/feed'}`
     .beforeEach(async () => {
       await waitForReact();
     });
